f73a0ad8fb388b2c123b93592d99f50299ff7d81
Jim Thorson <JamesT.esq@gmail.com>
2018-11-06 07:44:45 -0800

    Remove INLA from SUGGESTS...
    ... because its causing a warning, and therefore an error for users that convert warnings to errors

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,11 @@
 #}
 
 .onAttach <- function(libname, pkgname) {
-  #if( !"INLA" %in% utils::installed.packages()[,1] ){
-  #  packageStartupMessage("Installing INLA...")
-  #  utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
-  #}
+  if( !"INLA" %in% utils::installed.packages()[,1] ){
+    packageStartupMessage("Installing package: INLA...")
+    #utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
+    utils::install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+  }
   #if( !"TMB" %in% utils::installed.packages()[,1] ){
   #  packageStartupMessage("Installing TMB...")
   #  devtools::install_github("kaskr/adcomp/TMB")
