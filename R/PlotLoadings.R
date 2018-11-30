41b6cbee4d853dc8a8078ffb240779f43c4c2053
Jim Thorson <JamesT.esq@gmail.com>
2018-11-29 18:26:18 -0800

    small improvements to `PlotLoadings`

--- R/PlotLoadings.R
+++ R/PlotLoadings.R
@@ -18,10 +18,11 @@
 #' }
 
 #' @export
-PlotLoadings = function( L_pj, whichfactor=1, addtitle=TRUE, LabelPosition="Right", Buffer=c(0,0.1), Labels=rownames(L_pj), Cex=1.2 ){
+PlotLoadings = function( L_pj, whichfactor=1, addtitle=TRUE, LabelPosition="Right", Buffer=c(0,0.1), Labels=rownames(L_pj), Cex=1.2,
+  legend_text="Proportion of explained variance", ... ){
 
   # Plotting window
-  plot(1, type="n", xlim=c(0.5,nrow(L_pj)+0.5), ylim=range(L_pj)+diff(range(L_pj))*Buffer, xlab="", ylab="", xaxt="n", xaxs="i" )
+  plot(1, type="n", xlim=c(0.5,nrow(L_pj)+0.5), ylim=range(L_pj)+diff(range(L_pj))*Buffer, xlab="", ylab="", xaxt="n", xaxs="i", ... )
   if(LabelPosition=="Xaxis") axis( side=1, at=1:nrow(L_pj), labels=TRUE, las=3)
   if(addtitle==TRUE) mtext( text=paste("Factor",whichfactor), side=3, line=0.1, adj=0)
   abline(h=0)
@@ -33,5 +34,5 @@ PlotLoadings = function( L_pj, whichfactor=1, addtitle=TRUE, LabelPosition="Righ
     if(LabelPosition=="Above") text(x=p, y=max(L_pj), labels=Labels[p], srt=90, pos=3, cex=Cex)
     if(LabelPosition=="Below") text(x=p, y=min(L_pj), labels=Labels[p], srt=90, pos=1, cex=Cex)
   }
-  legend( "top", legend=paste0("Proportion of explained variance= ",round(100*sum(L_pj[,whichfactor]^2)/sum(L_pj^2),1),"%"), bty="n")
+  if(!is.null(legend_text)) legend( "top", legend=paste0(legend_text," ",round(100*sum(L_pj[,whichfactor]^2)/sum(L_pj^2),1),"%"), bty="n")
 }
