4b8f9436f785a7624014bac63b15df659e8fe9a4
James.Thorson <James.Thorson>
2019-03-18 20:11:20 +0000

    Fix bug in last commit

--- R/Prepare_BSslope_Extrapolation_Data_Fn.R
+++ R/Prepare_BSslope_Extrapolation_Data_Fn.R
@@ -15,7 +15,7 @@ function( strata.limits=NULL, zone=NA, flip_around_dateline=FALSE, ... ){
   Area_km2_x = Data_Extrap[,'Area_in_survey_km2']
   
   # Augment with strata for each extrapolation cell
-  Tmp = cbind("BEST_DEPTH_M"=Data_Extrap[,'Depth_km ']*1000, "BEST_LAT_DD"=Data_Extrap[,'Lat'], "propInSurvey"=1)
+  Tmp = cbind("BEST_DEPTH_M"=Data_Extrap[,'Depth_km']*1000, "BEST_LAT_DD"=Data_Extrap[,'Lat'], "propInSurvey"=1)
   a_el = as.data.frame(matrix(NA, nrow=nrow(Data_Extrap), ncol=nrow(strata.limits), dimnames=list(NULL,strata.limits[,'STRATA'])))
   for(l in 1:ncol(a_el)){
     a_el[,l] = apply(Tmp, MARGIN=1, FUN=match_strata_fn, strata_dataframe=strata.limits[l,,drop=FALSE])
