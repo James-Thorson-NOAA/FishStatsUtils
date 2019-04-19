e9f720258a75e2e66c781bbdf3962d983795ea60
James.Thorson <1454196437@mil>
2019-04-19 09:36:01 -0700

    updating make_settings...
    ... to allow for MICE in space model

--- R/make_settings.R
+++ R/make_settings.R
@@ -14,6 +14,7 @@
 #' @param use_anisotropy Boolean indicating whether to estimate two additional parameters representing geometric anisotropy
 #' @param vars_to_correct a character-vector listing which parameters to include for bias-correction, as passed to \code{TMBhelper::Optimize}
 #' @param treat_nonencounter_as_zero Boolean indicating whether to treat any year-category combination as having zero biomass when generating abundance indices and resulting compositional estimates
+#' @param n_categories number of categories in a multivariate model (only necessary to specify given some values for \code{purpose})
 #'
 #' @return Tagged list containing default settings for a given purpose, use \code{names} on output to see list of settings.
 #'
@@ -24,19 +25,20 @@
 make_settings = function( n_x, Region, purpose="index", fine_scale=TRUE,
   strata.limits=data.frame('STRATA'="All_areas"), zone=NA, FieldConfig, RhoConfig,
   OverdispersionConfig, ObsModel, bias.correct, Options, use_anisotropy,
-  vars_to_correct, Version, treat_nonencounter_as_zero ){
+  vars_to_correct, Version, treat_nonencounter_as_zero, n_categories, VamConfig ){
 
   # Get version
   if(missing(Version)) Version = FishStatsUtils::get_latest_version()
 
   # Index standardization
-  if( purpose=="index" ){
+  if( tolower(purpose) == "index" ){
     if( convert_version_name(Version) >= convert_version_name("VAST_v7_0_0") ){
       if(missing(FieldConfig)) FieldConfig = matrix( "IID", ncol=2, nrow=3, dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
     }else{
       if(missing(FieldConfig)) FieldConfig = c("Omega1"="IID", "Epsilon1"="IID", "Omega2"="IID", "Epsilon2"="IID")
     }
     if(missing(RhoConfig)) RhoConfig = c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0)
+    if(missing(VamConfig)) VamConfig = c("Method"=0, "Rank"=0, "Timing"=0)
     if(missing(OverdispersionConfig)) OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
     if(missing(ObsModel)) ObsModel = c(1,1)
     if(missing(bias.correct)) bias.correct = TRUE
@@ -46,13 +48,14 @@ make_settings = function( n_x, Region, purpose="index", fine_scale=TRUE,
   }
 
   # Condition and density
-  if( purpose=="condition_and_density" ){
+  if( tolower(purpose) == "condition_and_density" ){
     if( convert_version_name(Version) >= convert_version_name("VAST_v7_0_0") ){
       if(missing(FieldConfig)) FieldConfig = matrix( c(2,2,"IID",0,0,"IID"), ncol=2, nrow=3, dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
     }else{
       if(missing(FieldConfig)) FieldConfig = c("Omega1"=2, "Epsilon1"=2, "Omega2"=0, "Epsilon2"=0)
     }
     if(missing(RhoConfig)) RhoConfig = c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0)
+    if(missing(VamConfig)) VamConfig = c("Method"=0, "Rank"=0, "Timing"=0)
     if(missing(OverdispersionConfig)) OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
     if(missing(ObsModel)) ObsModel = c(1,4)
     if(missing(bias.correct)) bias.correct = TRUE
@@ -61,8 +64,25 @@ make_settings = function( n_x, Region, purpose="index", fine_scale=TRUE,
     if(missing(vars_to_correct)) vars_to_correct = c( "Index_cyl" )
   }
 
+  # Condition and density
+  if( tolower(purpose) %in% c("mice","interactions") ){
+    if( convert_version_name(Version) >= convert_version_name("VAST_v7_0_0") ){
+      if(missing(FieldConfig)) FieldConfig = matrix( c(n_categories,n_categories,"IID",n_categories,n_categories,"IID"), ncol=2, nrow=3, dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
+    }else{
+      if(missing(FieldConfig)) FieldConfig = c("Omega1"=n_categories, "Epsilon1"=n_categories, "Omega2"=n_categories, "Epsilon2"=n_categories)
+    }
+    if(missing(n_categories)) stop("Must supply `n_categories` when using purpose==`mice`")
+    if(missing(RhoConfig)) RhoConfig = c("Beta1"=3, "Beta2"=3, "Epsilon1"=4, "Epsilon2"=6)
+    if(missing(VamConfig)) VamConfig = c("Method"=2, "Rank"=n_categories, "Timing"=1)
+    if(missing(OverdispersionConfig)) OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
+    if(missing(ObsModel)) ObsModel = c(1,1)
+    if(missing(bias.correct)) bias.correct = TRUE
+    if(missing(Options)) Options =  c("SD_site_logdensity"=FALSE, "Calculate_Range"=FALSE, "Calculate_effective_area"=FALSE, "Calculate_Cov_SE"=FALSE, "Calculate_Fratio"=TRUE, "Estimate_B0"=TRUE )
+    if(missing(vars_to_correct)) vars_to_correct = c( "Index_cyl", "Bratio_cyl" )
+  }
+
   # Check for bad input
-  if( !( purpose %in% c("index","condition_and_density")) ){
+  if( !( tolower(purpose) %in% c("index","condition_and_density","mice")) ){
     stop("'purpose' is currently set up only for index-standardization models and correlations between condition and density")
   }
 
@@ -76,7 +96,8 @@ make_settings = function( n_x, Region, purpose="index", fine_scale=TRUE,
 
   # Bundle and export
   settings = list("Version"=Version, "n_x"=n_x, "Region"=Region, "strata.limits"=strata.limits, "zone"=zone, "FieldConfig"=FieldConfig, "RhoConfig"=RhoConfig,
-    "OverdispersionConfig"=OverdispersionConfig, "ObsModel"=ObsModel, "vars_to_correct"=vars_to_correct, "Options"=Options, "grid_size_km"=grid_size_km,
+    "VamConfig"=VamConfig, "OverdispersionConfig"=OverdispersionConfig, "ObsModel"=ObsModel, "vars_to_correct"=vars_to_correct,
+    "Options"=Options, "grid_size_km"=grid_size_km,
     "Method"=Method, "use_anisotropy"=use_anisotropy, "fine_scale"=fine_scale, "bias.correct"=bias.correct )
   return(settings)
 }
