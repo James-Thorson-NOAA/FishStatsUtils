f7bc02be6f60feff834a0bc28d274de3afc7dd10
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-07-27 13:06:22 -0700

    adding plot_variable...
    ... as improved replacement for PlotMap_Fn

--- R/plot_variable.R
+++ R/plot_variable.R
@@ -4,9 +4,20 @@
 #' @description
 #' \code{PlotMap_Fn} is a hidden function to plot a map and fill in regions with colors to represent intensity in an areal-interpretion of model results
 #'
-#' @inheritParams plot_maps
-#' @param plot_legend_fig Boolean, whether to plot a separate figure for the heatmap legend or not
+#' @inheritParams FishStatsUtils::plot_maps
+#' @inheritParams sp::CRS
+#' @inheritParams par
+#'
+#' @param Y_gt matrix where values for every column are plotted as a map
 #' @param land_color color for filling in land (use \code{land_color=rgb(0,0,0,alpha=0)} for transparent land)
+#' @param legend_x two numeric values (between 0 and 1) giving left and right-hand location of color legend
+#' @param legend_y two numeric values (between 0 and 1) giving bottom and top location of color legend
+#' @param map_list output from \code{FishStatsUtils::make_map_info}
+#' @param zlim range for defining bounds of color scale
+#' @param add boolean indicating whether to add plot to an existing panel figure, or to define a new panel figure
+#' @param outermargintext vector defining text to plot in outer margins of panel figure
+#' @param panel_labels vector defining titles to use for each panel; defaults to blank
+#'
 #' @param ... arguments passed to \code{par}
 #'
 #' @details
@@ -17,11 +28,15 @@
 
 #' @export
 plot_variable <-
-function( Y_gt, spatial_list, extrapolation_list, map_info, panel_labels, proj4string='+proj=longlat', map_resolution="medium",
-         file_name="density", working_dir=paste0(getwd(),"/"), Format="png", Res=200, textmargin="", add=FALSE, pch=15,
+function( Y_gt, map_list, panel_labels, projargs='+proj=longlat', map_resolution="medium",
+         file_name="density", working_dir=paste0(getwd(),"/"), Format="png", Res=200, textmargin="", add=FALSE,
          outermargintext=c("Eastings","Northings"), zlim, col, mar=c(0,0,2,0), oma=c(4,4,0,0),
-         Legend=list("use"=FALSE, "x"=c(10,30), "y"=c(10,30)), mfrow, plot_legend_fig=TRUE, land_color="grey",
+         legend_x=c(0,0.05), legend_y=c(0,0.4), mfrow, land_color="grey",
          ...){
+
+  ###################
+  # Settings and inputs
+  ###################
 
   # Check for problems and fill in missing stuff
   if( is.vector(Y_gt)){
@@ -30,11 +45,12 @@ function( Y_gt, spatial_list, extrapolation_list, map_info, panel_labels, proj4s
   if( missing(zlim)){
     zlim = range(Y_gt, na.rm=TRUE)
   }
-  if( missing(map_info) ){
+  if( missing(map_list) ){
     MapSizeRatio = c(3, 3)
   }else{
-    MapSizeRatio = map_info$MapSizeRatio
+    MapSizeRatio = map_list$MapSizeRatio
   }
+  Y_gt = Y_gt[ map_list$PlotDF[which(map_list$PlotDF[,'Include']>0),'x2i'], , drop=FALSE]
   if( missing(mfrow) ){
     mfrow = ceiling(sqrt(ncol(Y_gt)))
     mfrow = c( mfrow, ceiling(ncol(Y_gt)/mfrow) )
@@ -46,71 +62,86 @@ function( Y_gt, spatial_list, extrapolation_list, map_info, panel_labels, proj4s
     warning( "panel_labels and `ncol(Y_gt)` don't match: Changing panel_labels'")
     panel_labels = 1:ncol(Y_gt)
   }
-
-  # Plotting functions
-  if( missing(col)) col = colorRampPalette(colors=c("darkblue","blue","lightblue","lightgreen","yellow","orange","red"))
+  if( missing(col)){
+    col = colorRampPalette(colors=c("darkblue","blue","lightblue","lightgreen","yellow","orange","red"))
+  }
   if( is.function(col)) col = col(1000)
 
-  # Plot
+  # Location of extrapolation-grid cells
+  loc_g = map_list$PlotDF[which(map_list$PlotDF[,'Include']>0),c('Lon','Lat')]
+
+  # CRS for original and new projections
+  CRS_orig = sp::CRS( '+proj=longlat' )
+  CRS_proj = sp::CRS( projargs )
+
+  # Data for mapping
+  map_data = rnaturalearth::ne_countries(scale=switch(map_resolution, "low"=110, "medium"=50, "high"=10, 50 ))
+  map_data = sp::spTransform(map_data, CRSobj=CRS_proj)
+
+  ###################
+  # Make panel figure
+  ###################
+
+  # Define device
   Par = list( mfrow=mfrow, mar=mar, oma=oma, ...)
   if(Format=="png"){
     png(file=file.path(working_dir, paste0(file_name,".png")),
         width=Par$mfrow[2]*MapSizeRatio[2],
         height=Par$mfrow[1]*MapSizeRatio[1], res=Res, units='in')
+    on.exit( dev.off() )
   }
   if(Format=="jpg"){
     jpeg(file=paste0(FileName, paste0(file_name,".jpg")),
          width=Par$mfrow[2]*MapSizeRatio[2],
          height=Par$mfrow[1]*MapSizeRatio[1], res=Res, units='in')
+    on.exit( dev.off() )
   }
   if(Format%in%c("tif","tiff")){
     tiff(file=paste0(FileName, paste0(file_name,".tif")),
          width=Par$mfrow[2]*MapSizeRatio[2],
          height=Par$mfrow[1]*MapSizeRatio[1], res=Res, units='in')
+    on.exit( dev.off() )
   }
-    if(add==FALSE) par( Par )          # consider changing to Par=list() input, which overloads defaults a la optim() "control" input
-    for( tI in 1:ncol(Y_gt) ){
-      # Read extrapolation grid
-      if( is.numeric(extrapolation_list$zone) ){
-        CRS_orig = CRS( paste0("+proj=utm +units=km +zone=",extrapolation_list$zone-ifelse(extrapolation_list$flip_around_dateline,30,0)) )
-      }else{
-        CRS_orig = CRS(extrapolation_list$zone)
-      }
-      Points_orig = SpatialPointsDataFrame( coords=spatial_list$loc_g, data=data.frame("y"=Y_gt[,tI]), proj4string=CRS_orig )
+  if(add==FALSE) par( Par )
 
-      # Reproject to Lat-Long
-      Points_LongLat = spTransform( Points_orig, CRS('+proj=longlat') )
+  # Loop across columns (years)
+  for( tI in 1:ncol(Y_gt) ){
+    # Read extrapolation grid
+    Points_orig = sp::SpatialPointsDataFrame( coords=loc_g, data=data.frame("y"=Y_gt[,tI]), proj4string=CRS_orig )
 
-      # Re-project to plotting CRS
-      Points_proj = spTransform( Points_orig, CRS_proj )
+    # Reproject to Lat-Long
+    Points_LongLat = sp::spTransform( Points_orig, sp::CRS('+proj=longlat') )
 
-      # Interpolate to raster
-      # library(plotKML)
-      cell.size = mean(diff(Points_proj@bbox[1,]),diff(Points_proj@bbox[2,]))/floor(sqrt(nrow(Y_gt)))
-      Raster_proj = plotKML::vect2rast( Points_proj, cell.size=cell.size )
-      image( Raster_proj, col=col, zlim=zlim )
+    # Re-project to plotting CRS
+    Points_proj = sp::spTransform( Points_orig, CRS_proj )
 
-      # Plot maps using rnaturalearth
-      #library( rnaturalearth )
-      map_info = rnaturalearth::ne_countries(scale=switch( "na", "low"=110, "medium"=50, "high"=10, 50 ))
-      map_info = spTransform(map_info, CRSobj=CRS_proj)
-      plot( map_info, col=land_color, add=TRUE )              # #EAEAEA
+    # Interpolate to raster
+    # library(plotKML)
+    cell.size = mean(diff(Points_proj@bbox[1,]),diff(Points_proj@bbox[2,]))/floor(sqrt(nrow(Y_gt)))
+    Raster_proj = plotKML::vect2rast( Points_proj, cell.size=cell.size )
+    image( Raster_proj, col=col, zlim=zlim )
 
-      # Title and box
-      title( panel_labels[tI], line=0.1, cex.main=ifelse(is.null(Par$cex.main), 1.5, Par$cex.main), cex=ifelse(is.null(Par$cex.main), 1.5, Par$cex.main) )
-      box()
-    }
-    # Include legend
-    if( Legend$use==TRUE ){
-      xl = (100-Legend$x[1])/100*min(coordinates(Points_proj)[,1]) + (Legend$x[1])/100*max(coordinates(Points_proj)[,1])
-      xr = (100-Legend$x[2])/100*min(coordinates(Points_proj)[,1]) + (Legend$x[2])/100*max(coordinates(Points_proj)[,1])
-      yb = (100-Legend$y[1])/100*min(coordinates(Points_proj)[,2]) + (Legend$y[1])/100*max(coordinates(Points_proj)[,2])
-      yt = (100-Legend$y[2])/100*min(coordinates(Points_proj)[,2]) + (Legend$y[2])/100*max(coordinates(Points_proj)[,2])
-      plotrix::color.legend(xl=xl, yb=yb, xr=xr, yt=yt, legend=round(seq(zlim[1],zlim[2],length=4),1), rect.col=col, cex=1, align=c("lt","rb")[2], gradient="y")
-    }
-    # Margin text
-    if(add==FALSE) mtext(side=1, outer=TRUE, outermargintext[1], cex=1.75, line=par()$oma[1]/2)
-    if(add==FALSE) mtext(side=2, outer=TRUE, outermargintext[2], cex=1.75, line=par()$oma[2]/2)
-  if(Format %in% c("png","jpg","tif","tiff")) dev.off()
+    # Plot maps using rnaturalearth
+    sp::plot( map_data, col=land_color, add=TRUE )
+
+    # Title and box
+    title( panel_labels[tI], line=0.1, cex.main=ifelse(is.null(Par$cex.main), 1.5, Par$cex.main), cex=ifelse(is.null(Par$cex.main), 1.5, Par$cex.main) )
+    box()
+  }
+
+  # Include legend
+  if( all(is.numeric(c(legend_x,legend_y))) ){
+    xl = (1-legend_x[1])*par('usr')[1] + (legend_x[1])*par('usr')[2]
+    xr = (1-legend_x[2])*par('usr')[1] + (legend_x[2])*par('usr')[2]
+    yb = (1-legend_y[1])*par('usr')[3] + (legend_y[1])*par('usr')[4]
+    yt = (1-legend_y[2])*par('usr')[3] + (legend_y[2])*par('usr')[4]
+    plotrix::color.legend(xl=xl, yb=yb, xr=xr, yt=yt, legend=round(seq(zlim[1],zlim[2],length=4),1), rect.col=col, cex=1, align=c("lt","rb")[2], gradient="y")
+  }
+
+  # Margin text
+  if(add==FALSE) mtext(side=1, outer=TRUE, outermargintext[1], cex=1.75, line=par()$oma[1]/2)
+  if(add==FALSE) mtext(side=2, outer=TRUE, outermargintext[2], cex=1.75, line=par()$oma[2]/2)
+
+  # return stuff as necessary
   return( invisible(list("Par"=Par)) )
 }
