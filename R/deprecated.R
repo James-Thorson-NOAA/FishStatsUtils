2ac17dab10590acd5c8698f65e66584a94b633ae
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-12-06 15:35:41 -0800

    small housekeeping...
    ... to move plot_lines(.) to deprecated.R

--- R/deprecated.R
+++ R/deprecated.R
@@ -1003,3 +1003,21 @@ function(MappingDetails, Mat, PlotDF, MapSizeRatio=c('Width(in)'=4,'Height(in)'=
   }
   return( invisible(list("Par"=Par)) )
 }
+
+
+#' @export
+plot_lines = function( x, y, ybounds, fn=lines, col_bounds="black", bounds_type="whiskers", border=NA,
+  border_lty="solid", lwd_bounds=1, ... ){
+
+  #warning( "`plot_lines` is soft-deprecated" )
+
+  fn( y=y, x=x, ... )
+  if( bounds_type=="whiskers" ){
+    for(t in 1:length(y)){
+      lines( x=rep(x[t],2), y=ybounds[t,], col=col_bounds, lty=border_lty, lwd=lwd_bounds)
+    }
+  }
+  if( bounds_type=="shading" ){
+    polygon( x=c(x,rev(x)), y=c(ybounds[,1],rev(ybounds[,2])), col=col_bounds, border=border, lty=border_lty)
+  }
+}
