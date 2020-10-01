444e56ddd5bc63c705dfefc72611ffc6a519b685
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2020-09-30 20:31:57 -0700

    Update plot_anisotropy.R

--- R/plot_anisotropy.R
+++ R/plot_anisotropy.R
@@ -51,19 +51,24 @@ plot_anisotropy = function( Obj,
     # Ellipses
     if( type=="ellipse" ){
       rss = function(V) sqrt(sum(V[1]^2+V[2]^2))
-      Pos_Major = Eigen$vectors[,1]*Eigen$values[1] * Report$Range_raw1
-      Pos_Minor = Eigen$vectors[,2]*Eigen$values[2] * Report$Range_raw1
-      Pres_Major = Eigen$vectors[,1]*Eigen$values[1] * Report$Range_raw2
-      Pres_Minor = Eigen$vectors[,2]*Eigen$values[2] * Report$Range_raw2
+      Major_1 = Minor_1 = Major_2 = Minor_2 = NA
+      if( !("logkappa1"%in%names(Map)) || !is.na(Map$logkappa1) ){
+        Major_1 = Eigen$vectors[,1]*Eigen$values[1] * Report$Range_raw1
+        Minor_1 = Eigen$vectors[,2]*Eigen$values[2] * Report$Range_raw1
+      }
+      if( !("logkappa2"%in%names(Map)) || !is.na(Map$logkappa2) ){
+        Major_2 = Eigen$vectors[,1]*Eigen$values[1] * Report$Range_raw2
+        Minor_2 = Eigen$vectors[,2]*Eigen$values[2] * Report$Range_raw2
+      }
       png(file=FileName, width=ControlList$Width, height=ControlList$Height, res=ControlList$Res, units=ControlList$Units)
         par( mar=c(3,3,2,0), mgp=c(1.25,0.25,0), tck=-0.02)
-        Range = 1.1 * c(-1,1) * max(abs( cbind(Pos_Major,Pos_Minor, Pres_Major,Pres_Minor) ))
+        Range = 1.1 * c(-1,1) * max(abs( cbind(Major_1,Minor_1, Major_2,Minor_2) ),na.rm=TRUE)
         plot( 1, type="n", xlim=Range, ylim=c(Range[1],Range[2]*1.2), xlab="", ylab="")
-        if( !("logkappa1"%in%names(Map)) || !is.na(Map$logkappa1) ){
-          shape::plotellipse( rx=rss(Pres_Major), ry=rss(Pres_Minor), angle=-1*(atan(Pres_Major[1]/Pres_Major[2])/(2*pi)*360-90), lcol="green", lty="solid")
+        if( !any(is.na(Major_1)) ){
+          shape::plotellipse( rx=rss(Major_1), ry=rss(Minor_1), angle=-1*(atan(Major_1[1]/Major_1[2])/(2*pi)*360-90), lcol="green", lty="solid")
         }
-        if( !("logkappa2"%in%names(Map)) || !is.na(Map$logkappa2) ){
-          shape::plotellipse( rx=rss(Pos_Major), ry=rss(Pos_Minor), angle=-1*(atan(Pos_Major[1]/Pos_Major[2])/(2*pi)*360-90), lcol="black", lty="solid")
+        if( !any(is.na(Major_2)) ){
+          shape::plotellipse( rx=rss(Major_2), ry=rss(Minor_2), angle=-1*(atan(Major_2[1]/Major_2[2])/(2*pi)*360-90), lcol="black", lty="solid")
         }
         title( "Distance at 10% correlation" )
         mtext(side=1, outer=FALSE, line=2, text="Eastings (km.)")
