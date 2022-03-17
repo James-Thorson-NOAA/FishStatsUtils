906fa9236bb9891cf8cb5dea90abf8dcfe37b0f5
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2022-03-17 07:34:10 -0700

    Updating AFSC grids for EBS / NBS and adding Chukchi ...
    ... h/t Jason Conner for these improvements!

--- R/Prepare_XXXX_Extrapolation_Data_Fn.R
+++ R/Prepare_XXXX_Extrapolation_Data_Fn.R
@@ -145,7 +145,7 @@ function( strata.limits=NULL, projargs=NA, zone=NA, flip_around_dateline=TRUE, .
 
   # Extra junk
   Data_Extrap = cbind( Data_Extrap, 'Include'=1)
-  Data_Extrap[,c('E_km','N_km')] = tmpUTM[,c('X','Y')]
+  Data_Extrap = cbind( Data_Extrap, "E_km"=tmpUTM[,'X'], "N_km"=tmpUTM[,'Y'] )
 
   # Return
   Return = list( "a_el"=a_el, "Data_Extrap"=Data_Extrap, "zone"=attr(tmpUTM,"zone"), "projargs"=attr(tmpUTM,"projargs"),
@@ -325,7 +325,7 @@ function( strata.limits=NULL, projargs=NA, zone=NA, flip_around_dateline=FALSE, 
   Area_km2_x = Data_Extrap[,'Area_in_survey_km2']
 
   # Augment with strata for each extrapolation cell
-  Tmp = cbind("BEST_DEPTH_M"=0, "BEST_LAT_DD"=Data_Extrap[,'Lat'], "propInSurvey"=1)
+  Tmp = cbind("BEST_DEPTH_M"=0, "BEST_LAT_DD"=Data_Extrap[,'Lat'], "BEST_LON_DD"=Data_Extrap[,'Lon'], "propInSurvey"=1)
   a_el = as.data.frame(matrix(NA, nrow=nrow(Data_Extrap), ncol=nrow(strata.limits), dimnames=list(NULL,strata.limits[,'STRATA'])))
   for(l in 1:ncol(a_el)){
     a_el[,l] = apply(Tmp, MARGIN=1, FUN=match_strata_fn, strata_dataframe=strata.limits[l,,drop=FALSE])
@@ -338,7 +338,45 @@ function( strata.limits=NULL, projargs=NA, zone=NA, flip_around_dateline=FALSE, 
 
   # Extra junk
   Data_Extrap = cbind( Data_Extrap, 'Include'=1)
-  Data_Extrap[,c('E_km','N_km')] = tmpUTM[,c('X','Y')]
+  Data_Extrap = cbind( Data_Extrap, "E_km"=tmpUTM[,'X'], "N_km"=tmpUTM[,'Y'] )
+
+  # Return
+  Return = list( "a_el"=a_el, "Data_Extrap"=Data_Extrap, "zone"=attr(tmpUTM,"zone"), "projargs"=attr(tmpUTM,"projargs"),
+    "flip_around_dateline"=flip_around_dateline, "Area_km2_x"=Area_km2_x)
+  return( Return )
+}
+
+#' @export
+Prepare_Chukchi_Extrapolation_Data_Fn <-
+function( strata.limits=NULL, projargs=NA, zone=NA, flip_around_dateline=FALSE, ... ){
+  # Infer strata
+  if( is.null(strata.limits)){
+    strata.limits = data.frame('STRATA'="All_areas")
+  }
+  message("Using strata ", strata.limits)
+
+  # Read extrapolation data
+  utils::data( chukchi_sea_grid, package="FishStatsUtils" )
+  Data_Extrap <- chukchi_sea_grid
+
+  # Survey areas
+  Area_km2_x = Data_Extrap[,'Area_in_survey_km2']
+
+  # Augment with strata for each extrapolation cell
+  Tmp = cbind("BEST_DEPTH_M"=0, "BEST_LAT_DD"=Data_Extrap[,'Lat'], "BEST_LON_DD"=Data_Extrap[,'Lon'], "propInSurvey"=1)
+  a_el = as.data.frame(matrix(NA, nrow=nrow(Data_Extrap), ncol=nrow(strata.limits), dimnames=list(NULL,strata.limits[,'STRATA'])))
+  for(l in 1:ncol(a_el)){
+    a_el[,l] = apply(Tmp, MARGIN=1, FUN=match_strata_fn, strata_dataframe=strata.limits[l,,drop=FALSE])
+    a_el[,l] = ifelse( is.na(a_el[,l]), 0, Area_km2_x)
+  }
+             #
+  # Convert extrapolation-data to an Eastings-Northings coordinate system
+  #tmpUTM = Convert_LL_to_UTM_Fn( Lon=Data_Extrap[,'Lon'], Lat=Data_Extrap[,'Lat'], zone=zone, flip_around_dateline=flip_around_dateline)                                                         #$
+  tmpUTM = project_coordinates( X=Data_Extrap[,'Lon'], Y=Data_Extrap[,'Lat'], projargs=projargs, zone=zone, flip_around_dateline=flip_around_dateline)                                                         #$
+
+  # Extra junk
+  Data_Extrap = cbind( Data_Extrap, 'Include'=1)
+  Data_Extrap = cbind( Data_Extrap, "E_km"=tmpUTM[,'X'], "N_km"=tmpUTM[,'Y'] )
 
   # Return
   Return = list( "a_el"=a_el, "Data_Extrap"=Data_Extrap, "zone"=attr(tmpUTM,"zone"), "projargs"=attr(tmpUTM,"projargs"),
